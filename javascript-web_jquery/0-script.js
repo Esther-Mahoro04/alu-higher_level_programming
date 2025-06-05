689dd82530a78b02702831deb15e7f14cb29cf48
Esther-Mahoro04 <e.mahoro@alustudent.com>
2025-06-05 20:06:54 +0000

    git commit

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// This JavaScript script updates the text color of the <header> element to red.
+
+// Use document.querySelector to select the <header> tag
+const header = document.querySelector('header');
+
+// Update the text color of the <header> element to red (#FF0000)
+header.style.color = '#FF0000';
+
